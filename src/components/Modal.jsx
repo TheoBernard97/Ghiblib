107f0f69a7f6e204ac62b2c2108247bf7d77f6d5
Théo Bernard <theo.bernard.94100@gmail.com>
2020-05-31 15:53:53 +0200

    Responsive Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -14,36 +14,101 @@ const ModalWrapper = styled.div`
   height: 100%;
 
   .modal {
-    display: flex;
+    display: grid;
+    grid-template-columns: 40% 60%;
+    grid-template-rows: auto 1fr;
+    grid-template-areas:
+      "poster info"
+      "poster description";
     position: relative;
     margin: auto;
-    padding: 1% 0.5% 1% 1%;
-    width: 750px;
-    max-height: 425px;
+    padding: 1%;
+    width: 790px;
+    max-height: 475px;
     background-color: white;
     border: solid black 1px;
   }
 
-  img {
+  .modal-poster {
+    grid-area: poster;
     border: solid black 1px;
-    min-height: 425px;
-    min-width: 40%;
-    max-width: 40%;
+    width: 100%;
+    min-height: 475px;
+    max-height: 475px;
   }
 
   .modal-info {
+    grid-area: info;
     text-align: left;
-    margin-left: 3%;
-    padding-right: 0.5%;
-    overflow-y: scroll;
+    margin-left: 15px;
+
+    h1 {
+      margin-top: 0;
+    }
   }
 
-  h1 {
-    margin-top: 0;
-  }
-
-  p {
+  .modal-description {
+    grid-area: description;
+    text-align: left;
+    margin: 0 0 0 15px;
     font-size: 1.1rem;
+    overflow-y: scroll;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
+  @media all and (max-width: 944px) {
+    .modal {
+      grid-template-columns: 125px 1fr;
+      grid-template-rows: auto 1fr;
+      grid-template-areas:
+        "poster info"
+        "description description";
+      margin: auto;
+      width: 75%;
+      height: 45%;
+    }
+
+    .modal-poster {
+      min-height: 175px;
+      max-height: 175px;
+    }
+
+    .modal-description {
+      margin: 2% 0 0;
+    }
+  }
+  @media all and (max-width: 765px) {
+    .modal {
+      grid-template-columns: 100px 1fr;
+      width: 90%;
+      height: 70%;
+    }
+
+    .modal-poster {
+      min-height: 140px;
+      max-height: 140px;
+    }
+
+    .modal-info {
+      margin-left: 10px;
+
+      h1 {
+        font-size: 1.5rem;
+      }
+      h2 {
+        font-size: 1.3rem;
+      }
+      h3 {
+        font-size: 1.1rem;
+        margin-bottom: 0;
+      }
+    }
+
+    .modal-description {
+      font-size: 0.9rem;
+    }
   }
 `;
 
@@ -59,13 +124,16 @@ function Modal() {
           <img
             src={moviesReducer[modalReducer.renderMovie].imgUrl}
             alt="Movie poster"
+            className="modal-poster"
           />
           <div className="modal-info">
             <h1>{moviesReducer[modalReducer.renderMovie].title}</h1>
             <h2>{moviesReducer[modalReducer.renderMovie].director}</h2>
             <h3>{moviesReducer[modalReducer.renderMovie].release_date}</h3>
-            <p>{moviesReducer[modalReducer.renderMovie].description}</p>
           </div>
+          <p className="modal-description">
+            {moviesReducer[modalReducer.renderMovie].description}
+          </p>
         </div>
       </ClickAwayListener>
     </ModalWrapper>
